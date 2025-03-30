9d324f7f56ea8eaea76e1297a96fe8d57fc14818
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-03-30 20:43:19 +0100

    start of element

--- src/todo.js
+++ src/todo.js
@@ -15,36 +15,40 @@ mode.onclick = function () {
 let input = document.getElementById('input');
 let parent = document.getElementById('addElement');
 let check = document.getElementById('check');
+let element = document.getElementById('element');
+let text = document.getElementById('text');
 
-input.addEventListener("keydown", function(event) {
+input.addEventListener("keydown", function (event) {
 
-// Ajout du background à la div du "check" lorsque le quotta de caractères est atteint 
+    // Ajout du background à la div du "check" lorsque le quotta de caractères est atteint 
 
-    if(this.value.length < 4) {
+    if (this.value.length < 4) {
         check.style.backgroundImage = "none"; // le background est désactivé  
-    } else { 
+    } else {
         check.style.backgroundImage = "linear-gradient(135deg, #55DDFF 0%, #C058F3 100%)"; // le background est activé
     }
 
-// Ajoute la valeur de l'utilisateur
+    // Ajoute la valeur de l'utilisateur
 
-    if(event.key === "Enter") { 
+    if (event.key === "Enter") {
         let valeur = this.value;
-        
-// Vérifie si la longueur est suffisante
-        if(this.value.length >= 5) {
-            let enfant = document.createElement("p");
-            enfant.textContent = this.value;
-            parent.appendChild(enfant);
 
-            // Efface l'input après validation
-            this.value = "";
-            check.style.backgroundImage = "none";
+        for (let i = 0; i < 6; i++) {
+                 console.log(element);
 
-        } else {
-            check.style.backgroundImage = "none";
-            alert("Enter enough characters");
-            check.disabled = true; // Empêche la validation
+            // Vérifie si la longueur est suffisante
+            if (this.value.length >= 5) {
+                text.textContent = this.value;
+
+                // Efface l'input après validation
+                this.value = "";
+                check.style.backgroundImage = "none";
+
+            } else {
+                check.style.backgroundImage = "none";
+                alert("Enter enough characters");
+                check.disabled = true; // Empêche la validation
+            }
         }
     }
 });

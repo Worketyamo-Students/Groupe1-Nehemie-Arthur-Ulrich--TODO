e98d932b9af83f38c476769d14a390fb46f5c32c
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-07 09:35:23 +0100

    effet de zoom sur les filtres

--- assets/javascript/todo.js
+++ assets/javascript/todo.js
@@ -8,7 +8,7 @@ mode.onclick = function () {
     if (document.body.classList.contains("dark-theme")) {
         mode.src = "./assets/img/dark-option.svg";
     } else {
-        mode.src = "./assets/img/light-option.svg"
+        mode.src = "./assets/img/light-option.svg";
     }
 }
 
@@ -21,14 +21,17 @@ let check = document.getElementById('check');
 let element = document.getElementById('element'); // Modèle de l'élément ajouté
 let category = document.getElementById('category'); // Appel de la div d'après
 let checkImage = document.getElementById('checkImage');
-let all1 = document.getElementById("all1"); let all2 = document.getElementById("all2")
-let activate1 = document.getElementById("activate1"); let activate2 = document.getElementById("activate2")
-let completed1 = document.getElementById("completed1"); let completed2 = document.getElementById("completed2")
-let clear = document.getElementById("clear")
+let all1 = document.getElementById("all1"); 
+let all2 = document.getElementById("all2");
+let activate1 = document.getElementById("activate1"); 
+let activate2 = document.getElementById("activate2");
+let completed1 = document.getElementById("completed1"); 
+let completed2 = document.getElementById("completed2");
+let clear = document.getElementById("clear");
 
-let all = []//TABLEAU ALL
-let activate = []//TABLEAU ACTIVE
-let completed = []//TABLEAU COMPLETED
+let all = []; //TABLEAU ALL
+let activate = []; //TABLEAU ACTIVE
+let completed = []; //TABLEAU COMPLETED
 
 // Établit le compteur d'élément
 let count = 0; // compteur initialisé à 0
@@ -40,7 +43,7 @@ input.addEventListener("keydown", function (event) {
 
     if (this.value.length < 4) {
         check.style.backgroundImage = "none"; // le background est désactivé  
-        checkImage.style.display = "none" //----off du checkImage
+        checkImage.style.display = "none"; //----off du checkImage
     } else {
         checkImage.style.display = "flex"; // Affiche l'image check
         check.style.backgroundImage = "linear-gradient(135deg, #55DDFF 0%, #C058F3 100%)"; // le background est activé
@@ -60,8 +63,8 @@ input.addEventListener("keydown", function (event) {
         // Vérifie si la longueur est suffisante
         if (this.value.length >= 5) {
 
-            parent.style.display = 'flex'
-            category.style.display = 'flex'
+            parent.style.display = 'flex';
+            category.style.display = 'flex';
             // clonage de l'élément à chaque entrée de l'utilisateur
             let newElement = element.cloneNode(true); // ".cloneNode(true)" permet de cloner l'element en question ainsi que ses enfants
             newElement.style.display = "flex"; // permet l'affichage de l'élément
@@ -69,7 +72,7 @@ input.addEventListener("keydown", function (event) {
             let textElement = newElement.querySelector('#text');
 
             let newAll = all.push(newElement) //TABLEAU D'ELEMENTS
-            let nombreEl = document.getElementById("nombreEl")
+            let nombreEl = document.getElementById("nombreEl");
 
             if (textElement) {
                 textElement.textContent = this.value;
@@ -80,120 +83,120 @@ input.addEventListener("keydown", function (event) {
             // parent.insertBefore(newElement, category);
 
             //Ajout de l'icone Delete au Hover de la souris
-            let se = newElement.querySelector("#sousElement")
+            let se = newElement.querySelector("#sousElement");
 
-            let select = se.querySelector("#select")
-            let checkI = se.querySelector("#checkImage2") //Evenement click du check
+            let select = se.querySelector("#select");
+            let checkI = se.querySelector("#checkImage2"); //Evenement click du check
 
             let supp = document.createElement("img")
-            supp.src = "assets/img/exit-option.svg"; supp.classList = "cursor-pointer"
+            supp.src = "assets/img/exit-option.svg"; 
+            supp.classList = "cursor-pointer";
 
             newElement.addEventListener('mouseover', () => {
-                se.appendChild(supp)
+                se.appendChild(supp);
 
                 //Suppression au click
                 supp.addEventListener('click', () =>{
-                    parent.removeChild(newElement)
-                    all = all.filter(all => all !== newElement)
-                    nombreEl.innerHTML = all.length + " items left"
+                    parent.removeChild(newElement);
+                    all = all.filter(all => all !== newElement);
+                    nombreEl.innerHTML = all.length + " items left";
 
-                    completed = completed.filter(completed => completed !== newElement)
-                    activate = activate.filter(activate => activate !== newElement)
-                    count--
+                    completed = completed.filter(completed => completed !== newElement);
+                    activate = activate.filter(activate => activate !== newElement);
+                    count--;
 
-                }) 
-            })
+                }) ;
+            });
             //Retrait de l'icone delete quand on quitte le hover
             newElement.addEventListener('mouseleave', () => {
-                se.removeChild(supp)
-
-            })
+                se.removeChild(supp);
+            });
 
             // Evenement click de Select 
-            let active = true //Sert a verifier et initialiser l'etat
+            let active = true; //Sert a verifier et initialiser l'etat
 
             select.addEventListener('click', () => {
                 if(active){
                     select.style.background = "linear-gradient(135deg, #55DDFF 0%, #C058F3 100%)"
-                    checkI.style.display = "flex"
-                    textElement.style.textDecoration = "line-through"
-                    textElement.style.color = "#9495A5"
+                    checkI.style.display = "flex";
+                    textElement.style.textDecoration = "line-through";
+                    textElement.style.color = "#9495A5";
 
-                    let newcompleted = completed.push(newElement) //ADD ELMT TO COMPLETED
-                    activate = all.filter(e => !completed.includes(e))
+                    let newcompleted = completed.push(newElement); //ADD ELMT TO COMPLETED
+                    activate = all.filter(e => !completed.includes(e));
 
                     clear.addEventListener('click', () => { //DELETE WHEN WE CLICK ON CLEAR
-                        parent.removeChild(newElement)
+                        parent.removeChild(newElement);
                         // all = all.filter(all => all !== newElement)
-                        all = activate
+                        all = activate;
 
-                        nombreEl.innerHTML = all.length + " items left"
-                        completed = completed.filter(completed => completed !== newElement)
-                    })                     
+                        nombreEl.innerHTML = all.length + " items left";
+                        completed = completed.filter(completed => completed !== newElement);
+                    }) ;                    
 
 
                 } else{ //SI LE CLICK N'EST PAS ACTIF
-                    select.style.background = ""
-                    checkI.style.display = ""
-                    textElement.style.textDecoration = ""
-                    textElement.style.color = ""
+                    select.style.background = "";
+                    checkI.style.display = "";
+                    textElement.style.textDecoration = "";
+                    textElement.style.color = "";
 
-                    completed = completed.filter(completed => completed !== newElement)
+                    completed = completed.filter(completed => completed !== newElement);
 
-                    activate = all.filter(e => !completed.includes(e))
+                    activate = all.filter(e => !completed.includes(e));
 
                     clear.addEventListener('click', () => {
 
-                    })
+                    });
                 }
                 active = !active
 
                 completed1.addEventListener('click', () => { //evenement click du Active
-                    parent.innerHTML = ""
+                    parent.innerHTML = "";
                     all1.style.color = "var(--select-categories)"; all2.style.color = "var(--select-categories)"; activate1.style.color = "var(--select-categories)"; activate2.style.color = "var(--select-categories)", completed1.style.color = "#3A7CFD"; completed2.style.color = "#3A7CFD";
                     for(let i=0; i<completed.length; i++){
                         parent.appendChild(completed[i])
                     }
-                })
+                });
                 completed2.addEventListener('click', () => { //evenement click du Active
-                    parent.innerHTML = ""
+                    parent.innerHTML = "";
                     all1.style.color = "var(--select-categories)"; all2.style.color = "var(--select-categories)"; activate1.style.color = "var(--select-categories)"; activate2.style.color = "var(--select-categories)", completed1.style.color = "#3A7CFD"; completed2.style.color = "#3A7CFD";
                     for(let i=0; i<completed.length; i++){
                         parent.appendChild(completed[i])
                     }
-                })
+                });
                 activate1.addEventListener('click', () => {
-                    parent.innerHTML = ""
+                    parent.innerHTML = "";
                     all1.style.color = "var(--select-categories)"; all2.style.color = "var(--select-categories)"; activate1.style.color = "#3A7CFD"; activate2.style.color = "#3A7CFD", completed1.style.color = "var(--select-categories)"; completed2.style.color = "var(--select-categories)";
                     for(let i=0; i < activate.length; i++)(
                         parent.appendChild(activate[i])
                     )
-                })
+                });
                 activate2.addEventListener('click', () => {
-                    parent.innerHTML = ""
+                    parent.innerHTML = "";
                     all1.style.color = "var(--select-categories)"; all2.style.color = "var(--select-categories)"; activate1.style.color = "#3A7CFD"; activate2.style.color = "#3A7CFD", completed1.style.color = "var(--select-categories)"; completed2.style.color = "var(--select-categories)";
                     for(let i=0; i < activate.length; i++)(
                         parent.appendChild(activate[i])
                     )
-                })
-            })
+                });
+            });
 
             all1.addEventListener('click', () => {
-                parent.innerHTML = ""
+                parent.innerHTML = "";
                  all1.style.color = "#3A7CFD"; all2.style.color = "#3A7CFD"; activate1.style.color = "var(--select-categories)"; activate2.style.color = "var(--select-categories)", completed1.style.color = "var(--select-categories)"; completed2.style.color = "var(--select-categories)";
                 for(let i=0; i<all.length; i++){
                     parent.appendChild(all[i])
                 }
-            })
+            });
             all2.addEventListener('click', () => {
-                parent.innerHTML = ""
+                parent.innerHTML = "";
                 all1.style.color = "#3A7CFD"; all2.style.color = "#3A7CFD"; activate1.style.color = "var(--select-categories)"; activate2.style.color = "var(--select-categories)", completed1.style.color = "var(--select-categories)"; completed2.style.color = "var(--select-categories)";
                 for(let i=0; i<all.length; i++){
                     parent.appendChild(all[i])
                 }
-            })
+            });
 
-            nombreEl.innerHTML = all.length + " items left" //Decompte d'elements entrés    
+            nombreEl.innerHTML = all.length + " items left"; //Decompte d'elements entrés    
 
             //Efface l'input après validation
             input.value = ""; // permet de vider l'input après chaque entrée
@@ -208,4 +211,4 @@ input.addEventListener("keydown", function (event) {
             check.disabled = true; // Empêche la validation
         }
     }
-})
+});
